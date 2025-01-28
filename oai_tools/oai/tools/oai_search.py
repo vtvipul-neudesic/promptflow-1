8f92f2ec178ad970c6a4091d5fd394b824a2a8c7
Vipul Tripathi <Vipul.Tripathi@neudesic.com>
2025-01-28 17:08:53 +0530

    add connection accepting functionality in search tool

--- oai_tools/oai/tools/oai_search.py
+++ oai_tools/oai/tools/oai_search.py
@@ -1,17 +1,17 @@
 from promptflow import tool
 import requests
 import json
-import os
-
+from promptflow.connections import CustomConnection
 
 @tool
-def oai_search_tool(is_env_setup_successful: bool, user_prompt: str, max_tokens: int) -> dict:
-    if not is_env_setup_successful:
-        return False
-    headers = {"Content-Type": "application/json", "api-key": os.getenv("API_KEY")}
+def oai_search_tool(connection: CustomConnection, user_prompt: str, max_tokens: int) -> dict:
+    endpoint = connection.configs.get("oai_endpoint")
+    deployment_name = connection.configs.get("deployment_name")
+    api_key = connection.secrets.get("oai_api_key")
+    headers = {"Content-Type": "application/json", "api-key": api_key}
     data = {"prompt": user_prompt, "max_tokens": max_tokens}
     response = requests.post(
-        f"{os.getenv('ENDPOINT')}/openai/deployments/{os.getenv('DEPLOYMENT_NAME')}/completions?api-version=2022-12-01",
+        f"{endpoint}/openai/deployments/{deployment_name}/completions?api-version=2022-12-01",
         headers=headers,
         data=json.dumps(data),
     )
